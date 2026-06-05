04f3d0bcae70a58de2e73573679db16f65731120
korneevnaanastasia-beep <korneevna.anastasia@gmail.com>
2026-06-06 00:55:24 +0700

    Глобальный рефакторинг архитектуры Мафиози + новый агент Оценщик

--- writing-portal/app/services/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.services.xp_service import (
-    award_session_xp, update_streak, process_session_end,
-    calculate_xp_from_words, get_streak_multiplier, check_hp_decay,
-    STREAK_MIN_WORDS, TITLES
-)
